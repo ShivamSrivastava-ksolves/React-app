54cae0d1aba711e435534e5748111e474794e645
shivam <shivam.srivastava@ksolves.com>
2023-10-25 11:42:02 +0530

    started with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const name = <span>Hello this is with jsx</span>
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello welcome to my keeps app, {name}</h1>
+   
+
     </div>
   );
 }
